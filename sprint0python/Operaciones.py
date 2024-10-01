7e50fa0146479b916b915776db0960e8c4488f23
Lucía Méndez <lmendezr24@fpcoruna.afundacion.org>
2024-10-01 14:32:01 +0200

    Creada clase Operaciones

---
+++ sprint0python/Operaciones.py
@@ -0,0 +1,16 @@
+def sumar(num1, num2):
+    print(num1 + num2)
+def restar(num1, num2):
+    print(num1 - num2)
+def multiplicar(num1, num2):
+    print(num1 * num2)
+def dividir(num1, num2):
+    if num2 == 0:
+        print("No se puede dividir por cero.")
+    else:
+        print(num1 / num2)
+
+#sumar(2, 2)
+#restar(2,3)
+#multiplicar(2, 6)
+#dividir(2, 0)
