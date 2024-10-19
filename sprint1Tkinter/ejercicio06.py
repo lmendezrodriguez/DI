cfffe20b6eff3c700d91f0ae99c7aab71756ddfc
Lucía Méndez <lmendezr24@fpcoruna.afundacion.org>
2024-10-19 19:32:11 +0200

    Sprint1Tkinter ejercicio06.py: mejorar comentarios de código

--- sprint1Tkinter/ejercicio06.py
+++ sprint1Tkinter/ejercicio06.py
@@ -3,12 +3,14 @@ import tkinter as tk
 
 def cambiar_etiqueta():
     """
-     Al pulsar boton_seleccion refleja en el texto de etiqueta_fruta la fruta
-     seleccionada en la lista
-     """
-    seleccion = listbox.curselection()
-    etiqueta_fruta.config(
-        text=f"Fruta seleccionada: {listbox.get(seleccion[0])}")
+    Al pulsar el botón 'Mostrar selección', refleja en el texto de
+    etiqueta_fruta la fruta seleccionada en la lista.
+    """
+    seleccion = listbox.curselection()  # Obtener la selección actual
+    if seleccion:  # Comprobar si hay una selección
+        etiqueta_fruta.config(
+            text=f"Fruta seleccionada: {listbox.get(seleccion[0])}"
+        )
 
 
 # Crear ventana principal
@@ -16,26 +18,27 @@ root = tk.Tk()
 root.title("Ejemplo de Listbox")
 root.geometry("300x200")
 
-# Crear etiqueta de menú de Checkbutton
+# Crear etiqueta que indica el menú de frutas
 etiqueta_listbox = tk.Label(root, text="Lista de Frutas", pady=5)
 etiqueta_listbox.pack()
 
 # Crear lista de frutas
 lista_frutas = ['Manzana', 'Banana', 'Naranja']
 
-# Crear listbox
+# Crear Listbox y añadir frutas
 listbox = tk.Listbox(root, selectmode=tk.SINGLE, height=3)
 for fruta in lista_frutas:
     listbox.insert(tk.END, fruta)
 listbox.pack(pady=5)
 
-# Botón para mostrar selección
+# Botón para mostrar la selección
 boton_seleccion = tk.Button(root, text="Mostrar selección",
                             command=cambiar_etiqueta)
 boton_seleccion.pack(pady=5)
 
-# Etiqueta para mostrar fruta seleccionada
+# Etiqueta para mostrar la fruta seleccionada
 etiqueta_fruta = tk.Label(root, text="Fruta seleccionada: Ninguna")
 etiqueta_fruta.pack(pady=5)
 
+# Iniciar el bucle principal de la interfaz gráfica
 root.mainloop()
