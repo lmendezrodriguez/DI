233adf02a684e77d82070a0bfb70917ad5893954
Lucía Méndez <lmendezr24@fpcoruna.afundacion.org>
2024-11-11 13:01:09 +0100

    Fase 3: modelo -> corregir errores al crear matriz board

--- sprint3TkinterJuegoMemoria/modelo.py
+++ sprint3TkinterJuegoMemoria/modelo.py
@@ -27,6 +27,8 @@ class GameModelo:
         self.hidden = None
         self.images = {}
         self.images_loaded = False
+        self._generate_board_()
+        self._load_images()
 
     def __str__(self):
         return (
@@ -43,19 +45,15 @@ class GameModelo:
         else:
             messagebox.showerror("Error",
                                  "La dificultad no ha sido cargada")
-        # Calcula el número total de celdas
-        num_cells = board_size ** 2
-
         # Genera identificadores de pares de cartas
-        num_pairs = board_size // 2
         image_ids = list(
-            range(1, num_pairs + 1)) * 2  # Cada par tiene el mismo ID
+            range(1, board_size * 2 + 1)) * 2  # Cada par tiene el mismo ID
 
         # Mezcla aleatoriamente los identificadores
         random.shuffle(image_ids)
 
         # Divide la lista de identificadores en una estructura 2D para el tablero
-        # Recorremos la lista de identificadores de cartas con un índice que va desde 0 hasta el total de celdas, en pasos de `board_size`
+        # Recorremos la lista de identificadores de cartas con un índice que va desde 0 hasta el tamaño del tablero, en pasos de `board_size`
         for i in range(0, board_size ** 2, board_size):
             # Creamos una sublista desde el índice actual `i` hasta `i + board_size`
             row = image_ids[i:i + board_size]
@@ -65,27 +63,32 @@ class GameModelo:
     def _load_images(self):
         def load_images_thread():
             # URL base de donde se descargarán las imágenes
-            base_url = "https://example.com/images/"  # URL base de las imágenes
+            base_url = "https://raw.githubusercontent.com/lmendezrodriguez/DI/refs/heads/main/sprint3TkinterJuegoMemoria/res/img/"  # URL base de las imágenes
             try:
                 # Cargar imagen oculta
 
                 hidden_image_url = f"{base_url}hidden.png"
-                self.hidden = recursos.descargar_imagen(hidden_image_url, self.cell_size)
+                self.hidden = recursos.descargar_imagen(hidden_image_url,
+                                                        self.cell_size)
 
                 # Cargar imágenes para cada identificador de carta en el tablero
                 unique_ids = set(id for row in self.board for id in row)
+                print(unique_ids)
                 for image_id in unique_ids:
                     image_url = f"{base_url}{image_id}.png"
-                    self.images[image_id] = recursos.descargar_imagen(image_url, self.cell_size)
+                    self.images[image_id] = recursos.descargar_imagen(
+                        image_url, self.cell_size)
+                print(self.images)
 
             except IOError as e:
                 messagebox.showerror("Error",
                                      "La imagen no ha sido cargada")
             self.images_loaded = True
+
         threading.Thread(target=load_images_thread, daemon=True).start()
 
     def images_are_load(self):
-        pass
+        return self.images_loaded
 
     def start_time(self):
         pass
