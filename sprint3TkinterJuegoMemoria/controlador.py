4557b99e78f056e0ff2fbde452d6d41057ab37a0
Lucía Méndez <lmendezr24@fpcoruna.afundacion.org>
2024-11-14 21:34:09 +0100

    Mejorar manejo de on_card_click para evitar volteos de más

--- sprint3TkinterJuegoMemoria/controlador.py
+++ sprint3TkinterJuegoMemoria/controlador.py
@@ -2,6 +2,8 @@ import json
 import tkinter as tk
 from tkinter import Radiobutton, Button, messagebox, ttk
 
+from select import select
+
 from modelo import GameModelo
 from vista import MainMenu, GameView
 
@@ -13,12 +15,14 @@ class GameController:
 
         :param root: Referencia a la ventana principal de Tkinter.
         """
+
         self.loading_window = None  # Ventana de carga que se mostrará mientras se cargan las imágenes
         self.game_view = None  # Vista del juego
         self.root = root  # Ventana principal de la aplicación
         self.model = GameModelo  # Modelo del juego, aún no inicializado
         self.selected = []  # Lista de cartas seleccionadas
         self.timer_started = False  # Bandera para el inicio del temporizador
+        self.block_clicks = False # Bandera para bloqueo de cartas
 
         # Crear el menú principal y pasarle las funciones callback
         self.main_menu = MainMenu(
@@ -32,16 +36,16 @@ class GameController:
         """Inicia una nueva partida."""
 
         # Mostrar la selección de dificultad y esperar a que el usuario seleccione
-        self.show_difficulty_selection()
+        selected_difficulty = self.main_menu.show_difficulty_selection()
 
         # Si existe un nombre de jugador y dificultad seleccionada, crear el modelo del juego
-        if self.selected_difficulty is not None:
+        if selected_difficulty is not None:
             player_name = self.main_menu.ask_player_name()  # Solicita el nombre del jugador
             if player_name is None:
                 messagebox.showerror("Error",
                                      "No has seleccionado ningún nombre.")
             else:
-                self.model = GameModelo(self.selected_difficulty,
+                self.model = GameModelo(selected_difficulty,
                                         player_name)  # Crear el modelo con los datos seleccionados
                 self.show_loading_window(
                     "Cargando tablero")  # Muestra la ventana de carga
@@ -72,42 +76,6 @@ class GameController:
         """Cierra la aplicación."""
         self.root.quit()  # Cierra la ventana principal de la aplicación
 
-    def show_difficulty_selection(self):
-        """
-        Muestra una ventana para seleccionar la dificultad del juego.
-
-        Crea una ventana emergente para que el jugador seleccione una dificultad entre
-        fácil, medio y difícil.
-        """
-        difficulty_toplevel = tk.Toplevel(
-            self.root)  # Crea una nueva ventana emergente
-        difficulty_toplevel.geometry(
-            "200x200")  # Establece el tamaño de la ventana
-        label = tk.Label(difficulty_toplevel,
-                         text="Selecciona la dificultad")  # Etiqueta informativa
-        label.pack(pady=5)
-
-        var_difficulty = tk.IntVar()  # Variable para almacenar la selección
-        var_difficulty.set(1)  # Valor por defecto (fácil)
-
-        # Radiobuttons para elegir la dificultad
-        Radiobutton(difficulty_toplevel, text="Fácil", variable=var_difficulty,
-                    value=1).pack(pady=5)
-        Radiobutton(difficulty_toplevel, text="Medio", variable=var_difficulty,
-                    value=2).pack(pady=5)
-        Radiobutton(difficulty_toplevel, text="Difícil",
-                    variable=var_difficulty, value=3).pack(pady=5)
-
-        def choose_difficulty():
-            """Asigna la dificultad elegida y cierra la ventana emergente."""
-            self.selected_difficulty = var_difficulty.get()
-            difficulty_toplevel.destroy()  # Cierra el diálogo
-
-        Button(difficulty_toplevel, text="Elegir",
-               command=choose_difficulty).pack(
-            pady=5)  # Botón para confirmar la selección
-        difficulty_toplevel.grab_set()  # Hace que la ventana de selección de dificultad sea modal (bloquea la ventana principal)
-        difficulty_toplevel.wait_window()  # Espera a que se cierre la ventana
 
     def show_loading_window(self, message):
         """
@@ -146,33 +114,61 @@ class GameController:
 
         :param pos: La posición de la carta seleccionada en el tablero.
         """
+        # Si ya hay dos cartas seleccionadas o si estamos esperando a que las cartas se desvolteen, no hacer nada
+        if len(self.selected) >= 2 or self.block_clicks:
+            return
+
+        # Inicia el temporizador en el primer clic
         self.model.start_timer()
         self.update_time()
-        print(pos)
-        if len(self.selected) < 1:
-            self.selected.append(pos)
-            image_id = self.model.board[pos[0]][pos[1]]
-            image = self.model.images.get(image_id)
-            self.game_view.update_board(pos, image)
-        elif len(self.selected) < 2 and self.selected[0] != pos:
-            #self.game_view.window.attributes('-disabled', True)
-            self.selected.append(pos)
-            image_id = self.model.board[pos[0]][pos[1]]
-            image = self.model.images.get(image_id)
-            self.game_view.update_board(pos, image)
-            self.handle_card_selection()
+
+        # Mostrar la carta seleccionada
+        image_id = self.model.board[pos[0]][pos[1]]
+        image = self.model.images.get(image_id)
+
+        # Actualizar el tablero con la carta volteada
+        self.game_view.update_board(pos, image)
+
+        # Agregar la carta a la lista de seleccionadas
+        self.selected.append(pos)
+
+        # Si ya hay dos cartas seleccionadas, comparar y manejar la selección
+        if len(self.selected) == 2:
+            # Incrementar los movimientos
             self.model.moves += 1
             self.update_move_count(self.model.moves)
+
+            # Bloquear nuevos clics hasta que las cartas se comparen
+            self.block_clicks = True
+
+            # Verificar si las cartas coinciden
+            self.handle_card_selection()
+
+            # Verificar si el juego ha terminado
             self.check_game_complete()
 
     def handle_card_selection(self):
         """
-        Maneja la selección de una carta por parte del jugador.
+        Maneja la selección de dos cartas por parte del jugador y verifica si coinciden.
+        Si no coinciden, las voltea de nuevo después de un breve retraso.
         """
-        if not self.model.check_match(self.selected[0], self.selected[1]):
+        if self.model.check_match(self.selected[0], self.selected[1]):
+            # Si las cartas coinciden, no hacer nada (las cartas permanecen descubiertas)
+            self.selected.clear()
+        else:
+            # Si no coinciden, volver a voltear las cartas después de un pequeño retraso
             self.game_view.window.after(500, self.game_view.reset_cards,
                                         self.selected[0], self.selected[1])
-        self.selected.clear()
+            self.selected.clear()
+
+        # Después de un breve retraso, habilitar nuevamente los clics
+        self.game_view.window.after(500, self.enable_clicks)
+
+    def enable_clicks(self):
+        """
+        Permite hacer clic en las cartas nuevamente después de una breve espera.
+        """
+        self.block_clicks = False
 
     def update_move_count(self, moves):
         """
