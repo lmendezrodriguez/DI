2c7e229d57f9689532f065bce93d087f84d2b858
Lucía Méndez <lmendezr24@fpcoruna.afundacion.org>
2024-11-25 20:40:07 +0100

    Sprint4Android - E5: crear Bottom Navigation View Activity, MainActivy y dos Fragments

---
+++ sprint4Android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mycatalog;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupNavegacion();
+
+    }
+    private void setupNavegacion() {
+        BottomNavigationView buttomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_hostfragment);
+        NavigationUI.setupWithNavController(buttomNavigationView, navHostFragment.getNavController());
+    }
+}
