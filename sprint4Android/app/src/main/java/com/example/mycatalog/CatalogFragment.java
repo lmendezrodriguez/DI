1aec737b7eeaafaaffe5a4eccb950f97797821d1
Lucía Méndez <lmendezr24@fpcoruna.afundacion.org>
2024-11-26 13:41:53 +0100

    Sprint4Android - E5: añadir funcionalidad al botón para llevar a DetailActivity

--- sprint4Android/app/src/main/java/com/example/mycatalog/CatalogFragment.java
+++ sprint4Android/app/src/main/java/com/example/mycatalog/CatalogFragment.java
@@ -1,44 +1,39 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link CatalogFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Fragmento del catálogo.
  */
 public class CatalogFragment extends Fragment {
 
-
-    public CatalogFragment() {
-        // Required empty public constructor
-    }
-
-    // TODO: Rename and change types and number of parameters
-    public static CatalogFragment newInstance(String param1, String param2) {
-        CatalogFragment fragment = new CatalogFragment();
-
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflar el layout para este fragmento
+        View view = inflater.inflate(R.layout.fragment_catalog, container, false);
 
-        }
-    }
+        // Configurar el botón
+        Button button = view.findViewById(R.id.button_detail_fragment);
+        button.setOnClickListener(v -> {
+            // Iniciar la DetailActivity
+            Intent intent = new Intent(requireContext(), DetailActivity.class);
+            startActivity(intent);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_catalog, container, false);
+            // Finalizar la MainActivity
+            requireActivity().finish();
+        });
+
+        return view;
     }
 }
