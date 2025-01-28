b1a83eeb4260111568c47b0cf155d96f2efbd0ef
Lucía Méndez <lmendezr24@fpcoruna.afundacion.org>
2025-01-28 14:19:47 +0100

    Semana 2.Correción: corrige binding entre view y activity

--- proyecto2doTrim/pajareandoApp/app/src/main/java/com/lmr/pajareandoapp/views/BirdDetailActivity.java
+++ proyecto2doTrim/pajareandoApp/app/src/main/java/com/lmr/pajareandoapp/views/BirdDetailActivity.java
@@ -2,25 +2,15 @@ package com.lmr.pajareandoapp.views;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.lmr.pajareandoapp.databinding.DetailBirdBinding;
-import com.squareup.picasso.Picasso;
+import com.lmr.pajareandoapp.models.Bird;
 
-/**
- * Actividad para mostrar los detalles de un ave.
- */
 public class BirdDetailActivity extends AppCompatActivity {
 
-    /**
-     * Es llamado cuando la actividad se crea
-     *
-     * @param savedInstanceState representa el estado de la actividad antes de que se destruya     *
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,31 +22,22 @@ public class BirdDetailActivity extends AppCompatActivity {
         // Recupera el intent que inició esta actividad
         Intent intent = getIntent();
 
-        // Inicializa las vistas con los datos del intent
-        TextView commonNameTextView = binding.birdCommonName;
-        TextView scientificNameTextView = binding.birdScientificName;
-        TextView descriptionTextView = binding.birdDescription;
-        ImageView photoView = binding.birdImage;
-
-        // Verifica si el intent contiene datos, si contiene datos los muestra en las vistas
         if (intent != null) {
-            // Populate views with intent data
+            // Obtener datos del intent
             String commonName = intent.getStringExtra("Common_name");
-            commonNameTextView.setText(commonName);
-
             String scientificName = intent.getStringExtra("Scientific_name");
-            scientificNameTextView.setText(scientificName);
-
             String description = intent.getStringExtra("Description");
-            descriptionTextView.setText(description);
-
             String urlPhoto = intent.getStringExtra("Url_photo");
-            Picasso.get().load(urlPhoto).into(photoView);
 
-            if (commonName == null || scientificName == null || description == null || urlPhoto == null) {
+            if (commonName != null && scientificName != null && description != null && urlPhoto != null) {
+                // Crear objeto Bird con los datos
+                Bird bird = new Bird(commonName, scientificName, description, urlPhoto);
+
+                // Configurar el objeto Bird en el binding
+                binding.setBird(bird);
+            } else {
                 Toast.makeText(this, "Error al obtener los datos del ave.", Toast.LENGTH_SHORT).show();
             }
         }
     }
-
 }
